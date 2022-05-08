77bb44344318eca86db9efb985bf0c6af4c5d34b
Priyanka <priyankahansi95@gmail.com>
2022-05-08 19:16:08 +0530

    added:LinkedList

---
+++ src/com/bridgelabz/DataStructure.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        LinkedList<Integer> LinkedList=new LinkedList();
+        LinkedList.push(70);
+        LinkedList.push(30);
+        LinkedList.push(56);
+
+        LinkedList.print();
+    }
+}
+
