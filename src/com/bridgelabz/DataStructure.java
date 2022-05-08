e0503faf59b99820e637c8ed6d554e01b188dc55
Priyanka <priyankahansi95@gmail.com>
2022-05-08 21:16:08 +0530

    added:insert element

--- src/com/bridgelabz/DataStructure.java
+++ src/com/bridgelabz/DataStructure.java
@@ -3,13 +3,11 @@ package com.bridgelabz;
 public class DataStructure {
     public static void main(String[] args) {
         LinkedList<Integer> LinkedList=new LinkedList();
-        LinkedList.add(70);
-        LinkedList.add(30);
         LinkedList.add(56);
+        LinkedList.add(70);
+        LinkedList.insertBetween(30);
 
         LinkedList.print();
-
-
 
     }
 }
